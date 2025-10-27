9c0e8296b17508056bf6cb56b9d36d3d9b0283be
RaulLopez3108 <raul_leonardo20@outlook.es>
2025-10-27 13:50:26 -0700

    agregue los registros al admin site

--- generador_constancias/constancias/admin.py
+++ generador_constancias/constancias/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Participante,Evento,Plantilla
 # Register your models here.
+
+admin.site.register(Participante)
+admin.site.register(Evento)
+admin.site.register(Plantilla)
