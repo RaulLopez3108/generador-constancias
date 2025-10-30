d842767130cf3fb5edbed4ef513fdbc1f9d45cbe
RaulLopez3108 <raul_leonardo20@outlook.es>
2025-10-30 16:10:51 -0700

    agregue los views de lista de participantes

--- generador_constancias/constancias/views.py
+++ generador_constancias/constancias/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Participante,Evento
 # Create your views here.
+
+def lista_participantes(request):
+    participantes = Participante.objects.all().order_by('nombre_participante')
+
+    contexto = {
+        'participantes': participantes
+    }
+
+    return render(request,'constancias/lista_participantes.html',contexto)
+
+
+def detalle_participante(request,pk):
+    participante = get_object_or_404(participante,id_participante = pk)
+
+    evento_registrado = participante.evento_id
+
+    contexto = {
+        'participante':participante,
+        'evento': evento_registrado,
+    }
+
+    return render(request,'constancias/detalle_participante.html',contexto)
