8c102d6c76d690d70214a04e4978abc7fdaf1809
RaulLopez3108 <raul_leonardo20@outlook.es>
2025-11-04 17:22:45 -0700

    Cambios al admin

--- generador_constancias/constancias/admin.py
+++ generador_constancias/constancias/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Participante,Evento,Plantilla
-# Register your models here.
+from django.db.models import Min # Necesitas importar Min
 
-admin.site.register(Participante)
+class ParticipanteAdmin(admin.ModelAdmin):
+    list_display = ('nombre_participante', 'email_participante')
+    search_fields = ('nombre_participante', 'email_participante')
+
+    
+
+# Registrar el modelo con la clase modificada
+admin.site.register(Participante, ParticipanteAdmin)
+
+
 admin.site.register(Evento)
 admin.site.register(Plantilla)
