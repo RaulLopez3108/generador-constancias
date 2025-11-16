0b0582a644099e4bb4f8d1504456c4744509b9c4
RaulLopez3108 <raul_leonardo20@outlook.es>
2025-11-16 14:38:28 -0700

    Formulario para la generacion de constancias

---
+++ generador_constancias/constancias/forms.py
@@ -0,0 +1,23 @@
+# constancias/forms.py
+
+from django import forms
+from .models import Participante, Evento, Plantilla
+
+class GenerarConstanciaForm(forms.Form):
+    
+        
+    participante = forms.ModelChoiceField(
+        queryset=Participante.objects.all().order_by('nombre_participante'),
+        label="Seleccionar Participante"
+    )
+    
+    evento = forms.ModelChoiceField(
+        queryset=Evento.objects.all().order_by('-fecha_inicio'), 
+        label="Seleccionar Evento"
+    )
+    
+    
+    plantilla = forms.ModelChoiceField(
+        queryset=Plantilla.objects.filter(activa=True).order_by('nombre_plantilla'),
+        label="Seleccionar Plantilla"
+    )
